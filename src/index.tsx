e92b0f84ced70eb8d230c587710627d5a0691890
Antonio Guzman <aguzmanv09@gmail.com>
2021-09-30 14:58:54 -0500

    Create basic structure for react app

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './sass/index.scss';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <div>Cumplo</div>,
+  app,
+);
