930bb4c956877749334905083a78ecca7a29246d
Antonio Guzman <aguzmanv09@gmail.com>
2021-10-01 10:29:41 -0500

    Handle error in vote

--- src/pages/Vote/index.tsx
+++ src/pages/Vote/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
 
 import { useStateValue } from '../../context';
@@ -7,33 +7,52 @@ import './styles.scss';
 
 const Vote = () => {
   const history = useHistory();
+  const [error, setError] = useState(false);
   const params = useParams<{ id: string }>();
   const { candidates, dispatch } = useStateValue();
   const candidate = candidates?.find((item) => item.id === params.id);
 
   useEffect(() => {
     if (candidate) {
-      voteForCandidate({ id: params.id, candidates, dispatch });
+      voteForCandidate({ id: params.id, candidates, dispatch })
+        .then((data) => {
+          if (!data) {
+            setError(true);
+          }
+        });
     }
   }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      history.replace('/vote/registered');
-    }, 3000);
-  }, []);
+    if (error) {
+      setTimeout(() => {
+        history.replace('/');
+      }, 3000);
+    } else {
+      setTimeout(() => {
+        history.replace('/vote/registered');
+      }, 3000);
+    }
+  }, [error]);
 
   return (
     <section className='Vote slideInRight'>
-      <div className='Vote__body'>
-        <p><strong>+1</strong></p>
-        <h1>¡Gracias por tu voto!</h1>
-      </div>
-      <div className='Vote__footer'>
-        <p>Esperamos que a</p>
-        <p><strong>{candidate?.name}</strong></p>
-        <p>le vay muy bien</p>
-      </div>
+      {error && (
+        <p><strong>¡Ha ocurrido un error!</strong></p>
+      )}
+      {!error && (
+        <>
+          <div className='Vote__body'>
+            <p><strong>+1</strong></p>
+            <h1>¡Gracias por tu voto!</h1>
+          </div>
+          <div className='Vote__footer'>
+            <p>Esperamos que a</p>
+            <p><strong>{candidate?.name}</strong></p>
+            <p>le vay muy bien</p>
+          </div>
+        </>
+      )}
     </section>
   );
 };
