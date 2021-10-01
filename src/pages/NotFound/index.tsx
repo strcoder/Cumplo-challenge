7828d9576aa939c462bc5c9e3e7f996448e9c242
Antonio Guzman <aguzmanv09@gmail.com>
2021-10-01 10:34:41 -0500

    Update not found page and add styles

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './styles.scss';
 
 const NotFound = () => {
   return (
-    <section>
+    <section className='NotFound'>
+      <p><strong>404</strong></p>
       <h1>Not Found</h1>
     </section>
   );
